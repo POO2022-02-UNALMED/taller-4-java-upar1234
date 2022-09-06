78efb05a8e9462438f04fa8460114ae532ce4c59
upar1234 <93810971+upar1234@users.noreply.github.com>
2022-09-06 11:21:31 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,10 +1,23 @@
 package classroom;
 
-public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+public class Tipo {
 	
-	private int codigo;
-	private Tipo(int codigo) {
-		this.codigo = codigo;
+	public enum tipo {
+		DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+		
+		Tipo tipo;
+		private int codigo;
+		
+		tipo(int codigo) {
+			
+			this.codigo = codigo;
+		}
+	
 	}
+
+	public static final Tipo FUNDAMENTACION = null;
+	public static final Tipo DISCIPLINAR = null;
+
 }
+
+	
